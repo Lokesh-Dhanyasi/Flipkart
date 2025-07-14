800d5ad393c4231050b64e10129bcb6067dbe31b
Lokesh-Dhanyasi <dhanyasilokesh@gmail.com>
2025-07-14 16:51:52 +0530

    adding files

---
+++ Flipkart.java
@@ -0,0 +1,10 @@
+public class Flipkart
+{
+
+
+public static void main(String[]args)
+{
+
+System.out.println("Hello");
+}
+}
---
+++ Flipkart3.java
@@ -0,0 +1,8 @@
+public class Flipkart3
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Dog")
+	}
+}
+
